c6989be2335d00ede00d9a174613a022b923b3ee
Esther Kim <kim.esther117@gmail.com>
2022-12-12 15:52:08 -0800

    Created DiceGame class

--- dice_game.py
+++ dice_game.py
@@ -45,17 +45,68 @@ class Player:
         return self._die.roll()
 
 
-# class DiceGame:
+class DiceGame:
 
-#     def __init__(self, player, computer):
-#         self.player = player
-#         self.computer = computer
+    def __init__(self, player, computer):
+        self._player = player
+        self._computer = computer
 
-#     def play(self, player, computer):
-#         pass
+    def play(self):
+        print("====================================")
+        print("   Welcome to Roll the Dice Game!   ")
+        print("====================================")
+        while True:
+            self.play_round()
+            #TODO: implement game over
 
-#     def play_round(self, player, computer):
-#         pass
+    def play_round(self):
 
-#     def check_game_over(self):
-#         pass
+        self.print_round_welcome()
+
+        #Roll the dice
+        player_value = self._player.roll_die()
+        computer_value = self._computer.roll_die()
+
+        #Show the values
+        self.show_dice(player_value, computer_value)
+
+        #Determine winner and loser
+        if player_value > computer_value:
+            print("You won the round!")
+            self.update_counters(winner=self._player, loser=self._computer)
+        elif computer_value > player_value:
+            print("The computer won this round. Try again!")
+            self.update_counters(winner=self._computer, loser=self._player)
+        else:
+            print("It's a tie!")
+
+        self.show_counters()
+
+    def print_round_welcome(self):
+        print("============ New Round =============")
+        input("Press any key to roll the die!")
+
+    def show_dice(self, player_value, computer_value):
+        print(f"Your die: {player_value}")
+        print(f"Computer die: {computer_value}")
+
+    def update_counters(self, winner, loser):
+        winner.decrement_counter()
+        loser.increment_counter()
+
+    def show_counters(self):
+        print(f"Your counter: {self._player.counter}")
+        print(f"Computer counter: {self._computer.counter}")
+
+    def check_game_over(self):
+        pass
+
+
+player_die = Die()
+computer_die = Die()
+
+my_player = Player(player_die, is_computer=False)
+computer_player = Player(computer_die, is_computer=True)
+
+game = DiceGame(my_player, computer_player)
+game.play()
